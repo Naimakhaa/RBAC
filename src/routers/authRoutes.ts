e205b4336aeecc9bfded439cd3b96658b01a38fc
Kharizma-Rizkiah <h1101241050@student.untan.ac.id>
2026-05-04 05:33:43 +0000

    commit

--- src/routers/authRoutes.ts
+++ src/routers/authRoutes.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
-import { showLogin, login, logout } from "../controllers/authController";
+import { showLogin, login } from "../controllers/authController";
 
 const router = Router();
 
 router.get("/login", showLogin);
 router.post("/login", login);
-router.get("/logout", logout);
 
 export default router;
